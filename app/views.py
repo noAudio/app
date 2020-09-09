2f7fcf1f9819d0121595268c992dd9aeb747a478
Samuel Odongo <samieodd@gmail.com>
2020-09-09 17:45:20 +0300

    Created views for public files

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello world'
+    return render_template('public/index.html')
 
 @app.route('/about')
 def about():
