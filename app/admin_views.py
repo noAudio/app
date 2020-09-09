7eedec27b2adb1663a62cb9ec8ab6a0f6f04eff3
Samuel Odongo <samieodd@gmail.com>
2020-09-09 17:45:49 +0300

    Created admin views in seperate directory

--- app/admin_views.py
+++ app/admin_views.py
@@ -1,8 +1,9 @@
 from app import app
+from flask import render_template
 
 @app.route('/admin/dashboard')
 def admin_dashboard():
-    return 'Admin Dashboard'
+    return render_template('admin/dashboard.html')
 
 @app.route('/admin/profile')
 def admin_profile():
