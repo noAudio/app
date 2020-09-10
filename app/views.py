7d12a1216174373b909b957e1a1b912bbdbdd4d8
Samuel Odongo <samieodd@gmail.com>
2020-09-10 13:34:20 +0300

    Grabbing data using POST method

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect, jsonify, make_response
 from datetime import datetime
 
 @app.template_filter('clean_date')
@@ -62,3 +62,71 @@ def jinja():
 @app.route('/about')
 def about():
     return render_template('public/about.html')
+
+@app.route('/sign-up', methods=['GET', 'POST'])
+def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        print(username, email, password)
+
+        return redirect(request.url)
+    return render_template('public/sign_up.html')
+
+users = {
+    'mitshuhiko': {
+        'name': 'Armin Ronacher',
+        'bio': 'Creator of the Flask framework',
+        'twitter_handle': '@mitsuhiko'
+    },
+    'gvanrossum': {
+        'name': 'Guido Van Rossum',
+        'bio': 'Creator of the Python programming language',
+        'twitter_handle': '@gvanrossum'
+    },
+    'elonmusk': {
+        'name': 'Elon Musk',
+        'bio': 'Technology entrepreneur, investor and human rights violator.',
+        'twitter_handle': '@elonmusk'
+    },
+}
+
+@app.route('/profile/<username>')
+def profile(username):
+    user = None
+
+    if username in users:
+        user = users[username]
+
+    return render_template('public/profile.html', username=username, user=user)
+
+@app.route('/multiple/<foo>/<bar>/<baz>')
+def multiple(foo, bar, baz):
+    return f'Foo is {foo}, bar is {bar}, baz is {baz}.'
+
+@app.route('/json', methods=['POST'])
+def json():
+    if request.is_json:
+        req = request.get_json()
+        
+        response = {
+            'message': 'JSON received!',
+            'name': req.get('name')
+        }
+
+        res = make_response(jsonify(response), 200)
+        
+        return res
+    else:
+        res = make_response(jsonify({'message': 'No JSON received'}), 400)
+        return res
+
+@app.route('/guestbook')
+def guestbook():
+    return render_template('public/guestbook.html')
+
+@app.route('/guestbook/create-entry', methods=['POST'])
+def create_entry():
+    return 'Thanks'
